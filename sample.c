05fb2cd17bc2b11778108f858bfd33b458a90a2d
Hiroshi Osumi <naskin77@gmail.com>
2010-02-03 08:44:59 +0900

    simplify sample.c to read easily

--- sample.c
+++ sample.c
@@ -7,20 +7,10 @@
 int main(int argc, char *argv[]) {
 	unsigned long long int sec;
 
-	if (argc < 2) {
-		printf("usage: %s seconds\n", argv[0] );
-		exit(1);
-	}
+	sec = 120;
 
-	if (!isdigit(*argv[1])) {
-		fprintf(stderr, "argument is not numeric.\n");
-		exit(1);
-	}
 	printf("befor sleep\n");
-	sec = atol(argv[1]);
-
 	vsleep(sec);
-	
 	printf("after sleep\n");
 
 	return 0;
